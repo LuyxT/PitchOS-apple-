64f5fd411921802e1a22a05054dbc11efac00c5b
Tyler Tenger <tyler@tenger.de>
2026-02-07 21:02:27 +0100

    Initial Commit

---
+++ PitchInsights/PitchInsightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchInsightsApp.swift
+//  PitchInsights
+//
+//  Created by Tyler Tenger on 07.02.26.
+//
+
+import SwiftUI
+
+@main
+struct PitchInsightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
